311d6558a39e6ebd40e2ebf3c4f4780315d1f80c
morninng <mr_morninng_star_dash@yahoo.co.jp>
2015-04-22 00:22:05 +0900

    cloud code: saving thumbnail finish

--- Parse_Cloud_Code/CloudCode/cloud/main.js
+++ Parse_Cloud_Code/CloudCode/cloud/main.js
@@ -10,6 +10,10 @@ Parse.Cloud.define("cxense_dac_home", function(request, response) {
 	var date = new Date().toISOString();
 	var hmac = crypto.createHmac('sha256', apiKey).update(date).digest('hex');
 
+	var groups_array = ["concept"];
+	var filter = { "type":"keyword","group":"pageclass","item":"article"};
+	var filters_array = [filter];
+
 	Parse.Cloud.httpRequest({
 	  method: 'post',
 	  url: 'https://api.cxense.com/traffic/keyword',
@@ -20,7 +24,8 @@ Parse.Cloud.define("cxense_dac_home", function(request, response) {
 	  body: { 
 	  	start: -86400, 
 	  	siteId: '1128275557251903601',
-	  	historyResolution: 'minute',
+	  	groups: groups_array,
+	//  	filters: filters_array
 	  },
 	  success: function(httpResponse) {
 	     //response.success(httpResponse.text);
@@ -36,8 +41,8 @@ Parse.Cloud.define("cxense_dac_home", function(request, response) {
 	     		}
 	     	}
 	     }
-	     
-	     for(i=0;i<concept_item.length; i++){
+	     var i=0;
+	    for(i=0;i<concept_item.length; i++){
 	     	Save_TrafficInfo_OnParse(concept_item[i],i,concept_item.length, response);
 	     }
 	  },
@@ -53,6 +58,8 @@ Parse.Cloud.define("cxense_dac_home", function(request, response) {
 //"filters":[{"type":"keyword","group":"concept","item":"dac"}]}
 
 function Save_TrafficInfo_OnParse(str_concept, n,nn, response){
+
+	console.log("Save_TrafficInfo_OnParse" +str_concept);
 	var  crypto = require('crypto');
 	var username = 'cxense-team@dac.co.jp';
 	var apiKey = 'api&user&Qkc0a6QqYvTPjOsYbhR7Sg==';
@@ -91,14 +98,12 @@ function Save_TrafficInfo_OnParse(str_concept, n,nn, response){
 		site_concept.save().then(function(obj) {
 		  // the object was saved successfully.
 		  console.log("--site concept is saved --");
-
 		  RetrieveURL_list(site_concept.id, str_concept, response);
 
 		}, function(error) {
 		  // the save failed.
+		  console.log("parse data save failure");
 		});
-
-	 //   response.success(httpResponse.text);
 
 	  },
 	  error: function(httpResponse) {
@@ -110,8 +115,7 @@ function Save_TrafficInfo_OnParse(str_concept, n,nn, response){
 
 function RetrieveURL_list(site_concept_parse_id, str_concept, response){
 
-	console.log("RetrieveURL_list---------");
-	console.log(str_concept + "---");
+	console.log("RetrieveURL_list---------" + str_concept);
 
 	var  crypto = require('crypto');
 	var username = 'cxense-team@dac.co.jp';
@@ -121,8 +125,8 @@ function RetrieveURL_list(site_concept_parse_id, str_concept, response){
 	var groups_array = new Array("url");
 	var filter1 = { "type":"keyword","group":"concept","item":str_concept};
 	var filter2 = { "type":"keyword","group":"site","item":"dac.co.jp"};
-	var filter3 = { "type":"keyword","group":"pageclass","item":"article"};
-	var filters_array = [filter1,filter2,filter3];
+//	var filter3 = { "type":"keyword","group":"pageclass","item":"article"};
+	var filters_array = [filter1,filter2 /*,filter3 */];
 
 	Parse.Cloud.httpRequest({
 	  method: 'POST',
@@ -137,10 +141,28 @@ function RetrieveURL_list(site_concept_parse_id, str_concept, response){
 	  	groups: groups_array,
 	  	count: 20,
 	  	filters: filters_array
+	 // 	orderBy: 'uniqueUsers'
 	  },
 	  success: function(httpResponse) {
 	  	console.log("str concept is " + str_concept + "url list is" + httpResponse.text);
 	// 	response.success(httpResponse.text);
+
+	     var data_obj = JSON.parse(httpResponse.text);
+	     var groups_array = data_obj.groups;
+	     var URL_item_array = new Array();
+	     var URL_items_array = new Array();
+	     for(var i=0; i<groups_array.length; i++ ){
+	     	if(groups_array[i].group == "url"){
+	     		URL_items_array = groups_array[i].items;
+	     		for(var j=0; j<URL_items_array.length; j++){
+	     			URL_item_array.push(URL_items_array[j].item);
+	     		}
+	     	}
+	     }
+	     console.log("url_array is" + URL_item_array);
+	     for(var k=0; k<URL_item_array.length; k++ ){
+			 Save_EachPageInfo_OnParse(URL_item_array[k],str_concept,site_concept_parse_id);
+		 }
 	  },
 	  error: function(httpResponse) {
 	//    response.error('Request failed with response code ' + httpResponse.status);
@@ -150,15 +172,76 @@ function RetrieveURL_list(site_concept_parse_id, str_concept, response){
 }
 
 
-/*
-<Retrieve each context info>
-var content_fetch_query =  {"url":url};
-{"url":"","groups":["url","title","thumbnails"]}
-*/
+function Save_EachPageInfo_OnParse(in_url, str_concept, parse_site_concept_id ){
 
+	console.log("save each page info  " + in_url);
+	var  crypto = require('crypto');
+	var username = 'cxense-team@dac.co.jp';
+	var apiKey = 'api&user&Qkc0a6QqYvTPjOsYbhR7Sg==';
+	var date = new Date().toISOString();
+	var hmac = crypto.createHmac('sha256', apiKey).update(date).digest('hex');
+	var groups_array = ["url","title","thumbnails"];
 
-function Save_EachPageInfo_OnParse(site_concept_id, str_concept){
-	console.log(site_concept_id);
-	console.log(str_concept);
+	Parse.Cloud.httpRequest({
+	  method: 'post',
+	  url: 'https://api.cxense.com/profile/content/fetch',
+	  headers: { 
+	  	'X-cXense-Authentication': 'username=' + username + ' date=' + date + ' hmac-sha256-hex=' + hmac,
+	  	'Content-Type': 'application/json;charset=utf-8'
+	  },
+	  body: { 
+	  	url: in_url,
+	  	groups: groups_array
+	  },
+	  success: function(httpResponse) {
+		var data_obj = JSON.parse(httpResponse.text);
+		var site_url = data_obj.url;
+		var site_title = data_obj.title;
+		var image_height = 0;
+		var image_width = 0;
+		var image_url = "";
+
+		var SiteProfile = Parse.Object.extend("SiteProfile");
+		var site_profile = new SiteProfile();
+		site_profile.set("concept", str_concept);
+		site_profile.set("url", site_url);
+		site_profile.set("title", site_title);
+
+		var thumbnail_array = data_obj.thumbnails;
+		for(var l=0; l<thumbnail_array.length; l++){
+			if( thumbnail_array[l].type == "dominant"){
+				image_height = thumbnail_array[l].height;
+				image_width = thumbnail_array[l].width;	
+				image_url = thumbnail_array[l].url;
+				site_profile.set("image_height", image_height);
+				site_profile.set("image_width", image_width);
+				site_profile.set("image_url", image_url);
+			}
+		}
+		site_profile.save().then(function(obj) {
+
+			console.log("profile context save succeed");
+			var SiteConcept = Parse.Object.extend("SiteConcept");
+			var siteconcept_query = new Parse.Query(SiteConcept);
+		
+			return siteconcept_query.get(parse_site_concept_id);
+
+		}).then(function(site_concept_obj){
+			site_concept_obj.add("child",site_profile);
+
+			return site_concept_obj.save();
+		}).then(function(){
+			console.log("succeed");
+		}),function(error){
+			console.log(error);
+		};
+	  },
+	  error: function(httpResponse) {
+	//    response.error('Request failed with response code ' + httpResponse.status);
+	    console.log('Request failed with response code ' + httpResponse.status);
+	  }
+	});
 }
+
+
 
