e7453f3168412bc1d6707b7e7c3bbdd1ae949a78
morninng <mr_morninng_star_dash@yahoo.co.jp>
2015-08-27 00:40:15 +0900

    check box works

--- laravel/resources/views/matome_url_context.blade.php
+++ laravel/resources/views/matome_url_context.blade.php
@@ -5,6 +5,8 @@
  chose the article which you want to show on matome site<br>
 <hr>
 
+<button onclick="generate_matome()"> generate matome site </button>
+
 <span id="matome_list"></span>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
@@ -38,35 +40,52 @@
   flex:1;
 }
 
-
 </style>
+<script>
+function generate_matome(){
+
+  for( var i=0; i< keyword_list.length; i++){
+    var keyword = keyword_list[i];
+    console.log(keyword);
+    var url_content_array = eval("document." + keyword + ".elements");
+    for(var j=0; j<url_content_array.length; j++ ){
+      if( url_content_array[j].checked ){
+        console.log(url_content_array[j].value)
+      }
+    }
+  }
+}
+
+</script>
 <script type="text/template" data-template="matome_select_template">
  
 <h1> keyword is <%= keyword %> </h1>
-  <ul style="list-style:none;">
-     <% _.each(matome_list, function(e,i){ %>
-          <li>
-            <div class="matome_each_item">
-              <div class="matome_check_box_block">
-                  <input class="check_box" type="checkbox">
+  <form name = "<%= keyword %>">
+    <ul style="list-style:none;">
+       <% _.each(matome_list, function(e,i){ %>
+            <li>
+              <div class="matome_each_item">
+                <div class="matome_check_box_block">
+                    <input class="check_box" type="checkbox" value="<%= i %>">
+                </div>
+                <div class="matome_description">
+                  <p> title <strong><a href="<%= e.url %>"> <%= e.title %> </a></strong><br>
+                   description <%= e.description %><br>
+                   site name <%= e.site_name %></p>
+                  <p> 
+                    <span class="border_box"> pv &nbsp <%= e.pv %>&nbsp&nbsp&nbsp&nbsp</span>
+                    <span class="border_box"> active time :&nbsp <%= e.activeTime %>&nbsp&nbsp&nbsp&nbsp </span>
+                    <span class="border_box"> session bounce :&nbsp <%= e.sessionBounces %>&nbsp&nbsp&nbsp&nbsp </span>
+                  </p>
+                </div>
+                <div class="matome_image">
+                  <img src="<%= e.img_src %>" style="max-width:100px">
+                </div>
               </div>
-              <div class="matome_description">
-                <p> title <strong><a href="<%= e.url %>"> <%= e.title %> </a></strong><br>
-                 description <%= e.description %><br>
-                 site name <%= e.site_name %></p>
-                <p> 
-                  <span class="border_box"> pv &nbsp <%= e.pv %>&nbsp&nbsp&nbsp&nbsp</span>
-                  <span class="border_box"> active time :&nbsp <%= e.activeTime %>&nbsp&nbsp&nbsp&nbsp </span>
-                  <span class="border_box"> session bounce :&nbsp <%= e.sessionBounces %>&nbsp&nbsp&nbsp&nbsp </span>
-                </p>
-              </div>
-              <div class="matome_image">
-                <img src="<%= e.img_src %>" style="max-width:100px">
-              </div>
-            </div>
-          </li>
-     <% }); %>
-  </ul>
+            </li>
+       <% }); %>
+    </ul>
+  </form>
 
 </script>
 
@@ -74,29 +93,32 @@
   
 
   var matome_template = _.template($('[data-template="matome_select_template"]').html());
-
+  var keyword_list = new Array();
 <?php
   foreach ($matome_data_object_array as $matome_data_object){
 ?> 
   var matome_object = new Object();
   matome_object["keyword"] = "{{$matome_data_object['keyword']}}";
   var matome_list = new Array();
+  keyword_list.push("{{$matome_data_object['keyword']}}");
+
   <?php
     foreach ($matome_data_object["list_data"] as $matome_data){
   ?> 
-    var each_obj = new Object();
-    each_obj["title"] = "{{$matome_data->title}}";
-    each_obj["url"] = "{{$matome_data->url}}";
-    each_obj["img_src"] = "{{$matome_data->image}}";
-    each_obj["description"] = "{{$matome_data->description}}";
-    each_obj["site_name"] = "{{$matome_data->site_name}}";
-    each_obj["pv"] = "{{$matome_data->events}}";
-    each_obj["activeTime"] = "{{$matome_data->activeTime}}";
-    each_obj["sessionBounces"] = "{{$matome_data->sessionBounces}}";
-    matome_list.push(each_obj);
+      var each_obj = new Object();
+      each_obj["title"] = "{{$matome_data->title}}";
+      each_obj["url"] = "{{$matome_data->url}}";
+      each_obj["img_src"] = "{{$matome_data->image}}";
+      each_obj["description"] = "{{$matome_data->description}}";
+      each_obj["site_name"] = "{{$matome_data->site_name}}";
+      each_obj["pv"] = "{{$matome_data->events}}";
+      each_obj["activeTime"] = "{{$matome_data->activeTime}}";
+      each_obj["sessionBounces"] = "{{$matome_data->sessionBounces}}";
+      matome_list.push(each_obj);
   <?php
     }
   ?> 
+  console.log(matome_list);
   matome_object["matome_list"] = matome_list;
   var matome_html = matome_template(matome_object);
   $("#matome_list").append(matome_html);
